9416fd0a42262f77c3d9fa4952fab7c29229c7ad
charv-c <chenhangping@mail.nwpu.edu.cn>
2025-02-16 15:05:10 +0800

    添加棋子移动代码
    按索引移动，实现了移动动画，未实现索引和具体位置对应（留了对应方法），未实现移动触发

--- Assets/Scripts/Chess.cs
+++ Assets/Scripts/Chess.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Chess : MonoBehaviour
 {
+    public int ChessIndex;
+    public int ChessLocationIndex { get { return chesslocationindex; } }
+    int chesslocationindex;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +19,38 @@ public class Chess : MonoBehaviour
     void Update()
     {
         
+    }
+    public void Move(int delta)
+    {
+        int temp = 0;
+        while (temp != delta)
+        {
+            temp++;
+            MoveToIndex(temp+chesslocationindex);
+        }
+        chesslocationindex += temp;
+    }
+    void MoveToIndex(int index)
+    {
+        float duration = 0.2f;
+        Transform targettransform=BoardManager.Instance.Points[index];
+        StartCoroutine(MoveToTarget(targettransform.position, duration));
+    }
+    IEnumerator MoveToTarget(Vector2 target, float duration)
+    {
+        Vector2 startPosition = transform.position;
+        float elapsed = 0.0f;
+
+        while (elapsed < duration)
+        {
+            // 插值计算当前位置 lerp, calculate current location
+            float t = elapsed / duration;
+            transform.position = Vector2.Lerp(startPosition, target, t);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        // 确保最终到达目标位置 make sure get to target
+        transform.position = target;
     }
 }
